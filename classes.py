1aa7999419d62e99a991489450d02c9386c9daba
vishSP <hihi64741@gmail.com>
2023-03-15 15:49:08 +0200

    дописал  класс Superjob

--- classes.py
+++ classes.py
@@ -30,8 +30,7 @@ class HH(Engine):
     def get_request(self):
         return super().get_request(url=self.__url, params=self.__params).json()
 
-
-    def get_formatted_data(self,unformatted_data):
+    def get_formatted_data(self, unformatted_data):
 
         about_vacancy = {
             'site_name': 'HH.ru',
@@ -44,17 +43,16 @@ class HH(Engine):
         }
         return about_vacancy
 
-
-    def get_vacancy_lsit(self):
+    def get_vacancy_list(self):
 
         vacancy_list = []
         page = 0
         print('Идет поиск вакансий...')
         while True:
             self.__params['pages'] = page
+
             data = self.get_request()
             for item in data.get('items'):
-
                 vacancy_list.append(Vacancy(data=self.get_formatted_data(unformatted_data=item)))
             if data.get('pages') - page <= 1:
                 print(f'\nВакансий найдено:{len(vacancy_list)}')
@@ -70,13 +68,12 @@ class Superjob(Engine):
         self.my_auth_data = {
             'X-Api-App-Id': 'v3.r.137417367.c9eaf0a7d9ee18a7194f5c2b75fa46c86b533977.0146fbc82574c0cec8667869d1e560f196632d85'}
         self.__url = 'https://api.superjob.ru/2.0/vacancies'
-        self.__params = {"keywords": 'python', "page": 2}
+        self.__params = {"keywords": 'Python', "count": 100}
 
     def get_request(self):
         return super().get_request(url=self.__url, params=self.__params, headers=self.my_auth_data).json()
 
-    def get_formatted_data(self):
-        unformatted_data = self.get_request()
+    def get_formatted_data(self, unformatted_data):
 
         about_vacancy = {
             'site_name': 'Superjob.ru',
@@ -87,7 +84,28 @@ class Superjob(Engine):
             'city': get_city_sj(unformatted_data)
 
         }
+
         return about_vacancy
+
+    def get_vacancy_list(self):
+
+        vacancy_list = []
+        page = 1
+        print('Идет поиск вакансий...')
+        while True:
+            self.__params = {"keywords": 'Python', 'page': page, "count": 100}
+
+            data = self.get_request()
+            for item in data.get('objects'):
+                vacancy_list.append(Vacancy(data=self.get_formatted_data(unformatted_data=item)))
+            page += 1
+
+            if page == 6:
+                print(f'\nВакансий найдено: {len(vacancy_list)}')
+
+                break
+
+        return vacancy_list
 
 
 class Vacancy:
@@ -97,7 +115,8 @@ class Vacancy:
         self.data = data
 
     def __repr__(self):
-        return f'\nHH: {self.name}, зарплата {self.salary},\nОписание: {self.discription},\nСсылка: {self.url}\nгород: {self.city}\n'
+        return f'\n {self.name}, зарплата {self.salary},\nОписание: {self.discription},\nСсылка: {self.url}\nГород: {self.city}\n'
+
     @property
     def name(self):
         name = self.data['name']
@@ -124,9 +143,29 @@ class Vacancy:
         return city
 
 
+class HHVacancy(Vacancy):  # add counter mixin
+    """ HeadHunter Vacancy """
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return f'\nHH: {self.name}, зарплата {self.salary},\nОписание: {self.discription},\nСсылка: {self.url}\nгород: {self.city}\n'
+
+
+class SJVacancy(Vacancy):  # add counter mixin
+    """ SuperJob Vacancy """
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return f'\nSJ: {self.name}, зарплата {self.salary},\nОписание: {self.discription},\nСсылка: {self.url}\nгород: {self.city}\n'
+
+
 "выведи ссылку superjob"
 hh = HH()
 
 ss = Superjob()
 
-print(hh.get_vacancy_lsit())
+print(ss.get_vacancy_list())
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 def get_name_hh(data):
     name = data['name']
-
     return name
 
 
@@ -24,36 +23,51 @@ def get_salary_hh(data):
     if data['salary']['from'] is None:
         return f"до {data['salary']['to']}"
     else:
-       salary = f"от {data['salary']['from']} до {data['salary']['to']} руб/месяц"
+        salary = f"от {data['salary']['from']} до {data['salary']['to']} руб/месяц"
     return salary
 
 
 def get_city_hh(data):
-
     city = data['area']['name']
 
     return city
 
+
 def get_name_sj(data):
-    name = list(data).get('objects')[0].get('profession')
+
+    name = data['profession']
+
     return name
 
 
 def get_url_sj(data):
-    url = data.get('objects')[0].get('link')
+    url = data['link']
+
     return url
 
 
 def get_discription_sj(data):
-    discription = data.get('objects')[0].get('candidat').replace('\n', '').replace('•', '')
+    discription = data['candidat'].replace('\n', '').replace('•', '')
     return discription
 
 
 def get_salary_sj(data):
-    salary = f"{data.get('objects')[0].get('payment_from')} до {data.get('objects')[0].get('payment_to')}"
+    if data["payment_from"] == 0 and data["payment_to"] == 0:
+        return 'Договорная'
+    if data["payment_from"] == 0:
+        return f'до {data["payment_to"]} руб/месяц'
+    if data['payment_to'] == 0:
+        return f'{data["payment_from"]} руб/месяц'
+
+    else:
+        salary = f'{data["payment_from"]} до {data["payment_to"]} руб/месяц'
     return salary
 
 
 def get_city_sj(data):
-    city = data.get('objects')[0].get('address')
+    if data['address'] is None:
+        return data['town']['title']
+    else:
+        city = data['address']
     return city
+
